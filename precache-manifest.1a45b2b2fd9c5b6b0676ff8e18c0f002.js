364d99ec57f39abc0b1d4623a052c179c73cc86a
Shchepotin <vladislav.schepotin@gmail.com>
2019-05-09 14:48:02 +0000

    Updates

---
+++ precache-manifest.1a45b2b2fd9c5b6b0676ff8e18c0f002.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b317941f7bee607530c6",
+    "url": "/TimerTaskReact/static/js/runtime~main.9934f162.js"
+  },
+  {
+    "revision": "715a6c3444de7bde4686",
+    "url": "/TimerTaskReact/static/js/main.f55e2bd4.chunk.js"
+  },
+  {
+    "revision": "804a9177679f0ff4c6de",
+    "url": "/TimerTaskReact/static/js/2.b46c45ce.chunk.js"
+  },
+  {
+    "revision": "715a6c3444de7bde4686",
+    "url": "/TimerTaskReact/static/css/main.dfbbecc2.chunk.css"
+  },
+  {
+    "revision": "471a0c8c2dc8c3dd8824a58fcce8db93",
+    "url": "/TimerTaskReact/index.html"
+  }
+];
